4eec8637f313d4042b1f7c92aead842f1e923102
Saqib-002 <saqibshah2882@gmail.com>
2023-04-25 23:52:29 +0500

    Added rooms

--- sockets.js
+++ sockets.js
@@ -2,22 +2,27 @@ function listen(io){
     const pongNameSpace=io.of('/pong')
     let readyPlayerCount=0;
     pongNameSpace.on('connection',(socket)=>{
+        let room
         console.log(`a user connected as... ${socket.id}`);
         socket.on('ready',()=>{
+            room='room'+Math.floor(readyPlayerCount/2);
+            console.log(room)
+            socket.join(room);
             console.log('Player ready ',socket.id);
             readyPlayerCount++;
             if(readyPlayerCount%2===0){
-                pongNameSpace.emit('startGame',socket.id);
+                pongNameSpace.in(room).emit('startGame',socket.id);
             }
         })
         socket.on('paddleMove',(paddleData)=>{
-            socket.broadcast.emit('paddleMove',paddleData);
+            socket.to(room).emit('paddleMove',paddleData);
         } )
         socket.on('ballMove',(ballData)=>{
-            socket.broadcast.emit('ballMove',ballData);
+            socket.to(room).emit('ballMove',ballData);
         })
         socket.on('disconnect',(reason)=>{
             console.log(`Client ${socket.id} disconnected: ${reason}`);
+            socket.leave(room);
         })
     })
 }
